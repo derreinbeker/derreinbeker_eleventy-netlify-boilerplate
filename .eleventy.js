e473e3d7a7f3c1f737b0400dcfafceae228b2eba
Peer Sandtner <peer@peersandtner.de>
2019-04-26 13:28:53 +0200

    "readableDate" in eleventyConfig angepasst

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,7 @@ module.exports = function(eleventyConfig) {
 
   // Date formatting (human readable)
   eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy");
+    return DateTime.fromJSDate(dateObj).setLocale('de-DE').toFormat("d. LLLL yyyy");
   });
 
   // Date formatting (machine readable)

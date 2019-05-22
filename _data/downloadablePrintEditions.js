7aba706b4734f4c3d51df5ffbd8b8960451b8003
Peer Sandtner <peer@peersandtner.de>
2019-05-22 17:13:56 +0200

    minor cleanup...

--- _data/downloadablePrintEditions.js
+++ _data/downloadablePrintEditions.js
@@ -1,12 +1,9 @@
 const fetch = require("node-fetch");
 
 module.exports = async function() {
-  console.log( "Fetching Print-Ausgaben..." );
-
   return fetch("https://downloads.derreinbeker.de/ausgaben.json")
-    .then(res => res.json()) // node-fetch option to transform to json
+    .then(res => res.json())
     .then(json => {
-      // prune the data to return only what we want
       return json;
     });
 };

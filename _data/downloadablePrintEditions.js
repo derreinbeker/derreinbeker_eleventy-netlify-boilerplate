6f0bf7f6eeea8d73ece68afae0553032d963d8b8
Peer Sandtner <peer@peersandtner.de>
2019-12-06 14:40:18 +0100

    Das lassen wir mal lieber. Geht so auf netlify nicht... (Liste ist dann leer)

--- _data/downloadablePrintEditions.js
+++ _data/downloadablePrintEditions.js
@@ -1,12 +1,9 @@
 const fetch = require("node-fetch");
 
 module.exports = async function() {
-  if (!process.env.ELEVENTY_ENV == "development") {
-    return fetch("https://downloads.derreinbeker.de/ausgaben.json")
-    .then(res => res.json())
-    .then(json => {
-      return json;
-    });
-  }
-  return;
+  return fetch("https://downloads.derreinbeker.de/ausgaben.json")
+  .then(res => res.json())
+  .then(json => {
+    return json;
+  });
 };

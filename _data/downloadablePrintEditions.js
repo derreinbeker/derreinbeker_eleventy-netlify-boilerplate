7c4bc2b14e0bc5b6634dbb8ae696e7bc708b71ab
Peer Sandtner <peer@peersandtner.de>
2021-12-20 13:39:11 +0100

    Downloads: Vorbereitung für 2022 und 2023. Gleichzeitig 2020 und 2019 entfernt.

--- _data/downloadablePrintEditions.js
+++ _data/downloadablePrintEditions.js
@@ -1,6 +1,7 @@
 const fetch = require("node-fetch");
 
 module.exports = async function() {
+  // return fetch("http://makai.local/ausgaben.json")
   return fetch("https://downloads.derreinbeker.de/ausgaben.json")
   .then(res => res.json())
   .then(json => {

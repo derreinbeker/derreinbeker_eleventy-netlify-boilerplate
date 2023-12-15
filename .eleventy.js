42173d4c803e32d714b845ee7b7d7d317e296377
Peer Sandtner <peer@peersandtner.de>
2023-12-15 16:08:13 +0100

    Umbau on downloads auf liquid (hm, warum nur?)

--- .eleventy.js
+++ .eleventy.js
@@ -52,16 +52,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.setLibrary("md", markdownIt);
 
   return {
-    templateFormats: ["md", "njk", "html"],
-
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about it.
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for URLs (it does not affect your file structure)
     pathPrefix: "/",
 
-    htmlTemplateEngine: "njk",
-    passthroughFileCopy: true,
     dir: {
       input: ".",
       includes: "_includes",
